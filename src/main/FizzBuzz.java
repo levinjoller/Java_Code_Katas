2fd85b35b4d32d3838d85d977b13922ee0782ae3
LevinJoller <levin_joller@sluz.ch>
2021-05-04 16:40:23 +0200

    Fix code style

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -1,7 +1,7 @@
 /**
- * The FizzBuzz method getResult returns Fizz (%3 = 0), Buzz (%5 = 0) or
- * FizzBuzz ((%3 && %5) = 0).
- * 
+ * The FizzBuzz method getResult returns a number, Fizz (%3 == 0), Buzz (%5 == 0) or
+ * FizzBuzz ((%3 && %5) == 0).
+ *
  * @author Levin Joller
  * @version 1.0
  * @since 2021-05-04
@@ -11,6 +11,11 @@ package main;
 
 public class FizzBuzz {
 
+    /**
+     * This methods determines if a number a multiple of three, five or both.
+     *
+     * @return String This returns a number, Fizz, Buzz or FizzBuzz
+     */
     public static String getResult() {
         return "1";
     }
