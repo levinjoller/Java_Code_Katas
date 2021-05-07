10c8508e2d578687a5d9fbca045d538cc5ff871f
LevinJoller <levin_joller@sluz.ch>
2021-05-07 16:04:45 +0200

    Rename variables and constants

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -11,16 +11,16 @@ package main;
 
 public final class FizzBuzz {
 
-    /** Number three as a multiplier. */
-    private static final int MULTIPLIER_THREE = 3;
+    /** Number three. */
+    private static final int THREE = 3;
 
-    /** Number five as a multiplier. */
-    private static final int MULTIPLIER_FIVE = 5;
+    /** Number five. */
+    private static final int FIVE = 5;
 
-    /** Result for a multiple of three. */
-    private static final String MULTIPLE_THREE = "Fizz";
+    /** Name for a multiple of three. */
+    private static final String MULTIPLE_TREE = "Fizz";
 
-    /** Result for a multiple of five. */
+    /** Name for a multiple of five. */
     private static final String MULTIPLE_FIVE = "Buzz";
 
     private FizzBuzz() {
@@ -34,18 +34,18 @@ public final class FizzBuzz {
      * @return String; Returns a number, Fizz, Buzz or FizzBuzz.
      */
     public static String getResult(final int number) {
-        boolean isMultipleOfThree = (number % MULTIPLIER_THREE == 0);
-        boolean isMultipleOfFive = (number % MULTIPLIER_FIVE == 0);
+        boolean isMultipleOfThree = (number % THREE == 0);
+        boolean isMultipleOfFive = (number % FIVE == 0);
         String result = String.valueOf(number);
 
         if (isMultipleOfThree) {
-            result = MULTIPLE_THREE;
+            result = MULTIPLE_TREE;
         }
         if (isMultipleOfFive) {
             result = MULTIPLE_FIVE;
         }
         if (isMultipleOfThree && isMultipleOfFive) {
-            result = MULTIPLE_THREE + MULTIPLE_FIVE;
+            result = MULTIPLE_TREE + MULTIPLE_FIVE;
         }
         return result;
     }
