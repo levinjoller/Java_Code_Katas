b5bbe8900f3d444bc076ecb0a77e5e38abc27033
LevinJoller <levin_joller@sluz.ch>
2021-05-07 20:49:08 +0200

    Update to JUnit 5 🎉

--- src/test/FizzBuzzTest.java
+++ src/test/FizzBuzzTest.java
@@ -9,8 +9,8 @@
 package test;
 
 import main.FizzBuzz;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
