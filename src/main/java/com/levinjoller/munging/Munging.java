f3a69724694d86afad6b72bf158a9f4fe31ea564
LevinJoller <45421206+levinjoller@users.noreply.github.com>
2021-05-25 13:56:06 +0200

    Refactor munging kata

--- src/main/java/com/levinjoller/munging/Munging.java
+++ src/main/java/com/levinjoller/munging/Munging.java
@@ -36,9 +36,13 @@ public final class Munging {
         final int minStart = 12;
         final int minEnd = 14;
 
-        int day = Integer.parseInt(row.substring(dayStart, dayEnd));
-        int max = Integer.parseInt(row.substring(maxStart, maxEnd));
-        int min = Integer.parseInt(row.substring(minStart, minEnd));
+        String daySub = row.substring(dayStart, dayEnd);
+        String maxSub = row.substring(maxStart, maxEnd);
+        String minSub = row.substring(minStart, minEnd);
+
+        int day = Integer.parseInt(daySub);
+        int max = Integer.parseInt(maxSub);
+        int min = Integer.parseInt(minSub);
 
         Weather w = new Weather();
         w.setDay(day);
@@ -58,9 +62,9 @@ public final class Munging {
         final int firstRow = 2;
 
         try {
-            List<String> listWeather = Files.readAllLines(Paths.get(path));
+            List<String> Weathers = Files.readAllLines(Paths.get(path));
             for (int i = 0; i < 2; i++) {
-                String row = listWeather.get(i + firstRow);
+                String row = Weathers.get(i + firstRow);
                 result.add(toWeather(row));
             }
         } catch (IOException e) {
